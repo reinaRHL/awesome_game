9a699fe7c4d9c105aa15447a230aed5ff70d3e2c
Jay Svoboda <jay.svoboda@outlook.com>
2017-03-19 09:57:09 -0700

    add base node app

---
+++ web-app/src/server/app.js
@@ -0,0 +1,15 @@
+var bodyParser = require('body-parser');
+var express = require('express');
+var morgan = require('morgan');
+
+var app = express();
+var PORT = 3000;
+
+// Middleware
+app.use(morgan('dev'));
+
+// Routes
+
+app.listen(PORT, function () {
+	console.log('web-app started on port ' + PORT);
+});
