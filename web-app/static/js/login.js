cd91c10986e9adb1fa427add1293c4c36e78d912
James Araujo <jaraujo@sfu.ca>
2017-03-23 16:24:11 -0700

    added Login button, signup error-checking

---
+++ web-app/static/js/login.js
@@ -0,0 +1,30 @@
+// login js
+
+$(document).ready(function(){
+
+// swap password text field with basic text to make it 'visible'
+$("#password_show_checkbox").click(function () {
+ if ($(".password_show").attr("type")=="password") {
+   $(".password_show").attr("type", "text");
+ }
+ else{
+   $(".password_show").attr("type", "password");
+ }
+
+ });
+
+});
+
+function signupSubmit(){
+  var s = document.getElementById('errorContent');
+  var password = document.getElementById('pwSignup');
+  var password_confirm = document.getElementById('pwConfirm');
+
+  if(password.value === password_confirm.value){
+    // now submit the values to the DB as a user
+    s.innerHTML = "Correct";
+  }else{
+    // notify the user of incorrect credentials entered
+    s.innerHTML = "Passwords don't match.";
+  }
+}
