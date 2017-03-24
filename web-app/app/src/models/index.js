ea571c63282add87342647064f5c54fda717f00f
Jay Svoboda <jay.svoboda@outlook.com>
2017-03-23 18:50:55 -0700

    use sqlite for now

--- web-app/app/src/models/index.js
+++ web-app/app/src/models/index.js
@@ -1,26 +1,21 @@
-"use strict";
-
-var fs        = require("fs");
-var path      = require("path");
-var Sequelize = require("sequelize");
-var sequelize = new Sequelize(null, null, null,{dialect: 'mysql', storage: 'game.db'})
+var fs = require('fs');
+var path = require('path');
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize(null, null, null, { dialect: 'sqlite', storage: 'game.db' })
 
 var db = {};
 
-fs
-  .readdirSync(__dirname)
-  .filter(function(file) {
-    return (file.indexOf(".") !== 0) && (file !== "index.js");
-  })
-  .forEach(function(file) {
-    var model = sequelize.import(path.join(__dirname, file));
-    db[model.name] = model;
-  });
+fs.readdirSync(__dirname).filter(function (file) {
+	return (file.indexOf('.') !== 0) && (file !== 'index.js');
+}).forEach(function(file) {
+	var model = sequelize.import(path.join(__dirname, file));
+	db[model.name] = model;
+});
 
-Object.keys(db).forEach(function(modelName) {
-  if ("associate" in db[modelName]) {
-    db[modelName].associate(db);
-  }
+Object.keys(db).forEach(function (modelName) {
+	if ('associate' in db[modelName]) {
+		db[modelName].associate(db);
+	}
 });
 
 db.sequelize = sequelize;
