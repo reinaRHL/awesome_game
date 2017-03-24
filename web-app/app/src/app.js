499f4b4becb3a536702ce327d2a07f8e691dfe21
Jay Svoboda <jay.svoboda@outlook.com>
2017-03-23 18:33:36 -0700

    move src/server/ to src/ because client code is elsewhere

--- web-app/app/src/app.js
+++ web-app/app/src/app.js
@@ -1,13 +1,9 @@
 var bodyParser = require('body-parser');
 var express = require('express');
 var morgan = require('morgan');
-var path = require('path');
+
 var app = express();
 var PORT = 3000;
-app.use(bodyParser.json());       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({   // to support URL-encoded bodies
-  extended: true
-}));
 
 // Middleware
 app.use(morgan('dev'));
@@ -16,33 +12,6 @@ app.use(morgan('dev'));
 
 
 
-const server = app.listen(PORT, function () {
+app.listen(PORT, function () {
 	console.log('web-app started on port ' + PORT);
-});
-
-const io = require('socket.io')(server);
-
-var connections =[];
-
-io.on('connection', function (socket) {
-	connections.push(socket);
-	console.log("connected: %s sockets connected", connections.length);
-
-  	socket.on('disconnect', function (data) {
-		connections.splice(connections.indexOf(socket),1);
-		console.log("disconnected: %s sockets connected", connections.length);
-  	});
-
-  	// send message in Chatroom
-  	// need to fix this to pass the real username
-  	socket.on('send message', function(data){
-  		io.sockets.emit('new message', {msg:data, name:"Username"});
-  	})
-});
-
-
-/// This is for chatting feature 
-/// there's separate chat.html but it can be incoporated into lobby later
-app.get('/chat', function(req, res){
-	res.sendFile(path.resolve('../static/chat.html'));
 });
