b9a687d41bc803403ea45790dc379f219536f780
reina <reinal@sfu.ca>
2017-03-24 15:47:07 -0700

    basic setup for socket.io

--- web-app/app/src/app.js
+++ web-app/app/src/app.js
@@ -1,9 +1,13 @@
 var bodyParser = require('body-parser');
 var express = require('express');
 var morgan = require('morgan');
-
+var path = require('path');
 var app = express();
 var PORT = 3000;
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({   // to support URL-encoded bodies
+  extended: true
+}));
 
 // Middleware
 app.use(morgan('dev'));
@@ -17,3 +21,22 @@ const server = app.listen(PORT, function () {
 });
 
 const io = require('socket.io')(server);
+
+var connections =[];
+
+io.on('connection', function (socket) {
+	connections.push(socket);
+	console.log("connected: %s sockets connected", connections.length);
+
+  	socket.on('disconnect', function (data) {
+		connections.splice(connections.indexOf(socket),1);
+		console.log("disconnected: %s sockets connected", connections.length);
+  });
+});
+
+
+/// This is for chatting feature 
+/// there's separate chat.html but it can be incoporated into lobby later
+app.get('/chat', function(req, res){
+	res.sendFile(path.resolve('../../static/chat.html'));
+});
