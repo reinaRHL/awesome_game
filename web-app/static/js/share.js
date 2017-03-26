25e8d1437f8b951dfc2b67dfe861b8b52d37dcb7
reina <reinal@sfu.ca>
2017-03-26 11:45:14 -0700

    minor changes -indentation

--- web-app/static/js/share.js
+++ web-app/static/js/share.js
@@ -1,38 +1,33 @@
 $(document).ready(function(){
 
-  window.fbAsyncInit = function() {
-      FB.init({
-        appId      : '1006722016094891',
-        xfbml      : true,
-        version    : 'v2.8'
-      });
-      FB.AppEvents.logPageView();
+	window.fbAsyncInit = function() {
+    	FB.init({
+        	appId      : '1006722016094891',
+        	xfbml      : true,
+        	version    : 'v2.8'
+      	});
+      	
+      	FB.AppEvents.logPageView();
 
-      $('#shareBtn').click(function(){
-
-        FB.ui({
-      method: 'share',
-      display: 'popup',
-      href: 'http://imgur.com/1UuGwBL',
-      description: "This is awesome game! We can replace the link once game is hosted."
-    }, function(response){});
-      });
-
-
-$('#shareResultBtn').click(function(){
-
-       FB.ui({
-      method: 'share',
-      display: 'popup',
-      href: 'http://imgur.com/424rzw1',
-      description: "I won! We can replace the link once game is hosted."
-    }, function(response){});
-      });
+    	$('#shareBtn').click(function(){
+			FB.ui({
+		      method: 'share',
+		      display: 'popup',
+		      href: 'http://imgur.com/1UuGwBL',
+		      description: "This is awesome game! We can replace the link once game is hosted."
+    		}, function(response){});
+      	});
 
 
-
-    
-  };
+		$('#shareResultBtn').click(function(){
+			FB.ui({
+		      method: 'share',
+		      display: 'popup',
+		      href: 'http://imgur.com/424rzw1',
+		      description: "I won! We can replace the link once game is hosted."
+    		}, function(response){});
+      	});   
+  	};
 
 
     (function(d, s, id){
@@ -41,11 +36,7 @@ $('#shareResultBtn').click(function(){
        js = d.createElement(s); js.id = id;
        js.src = "https://connect.facebook.net/en_US/sdk.js";
        fjs.parentNode.insertBefore(js, fjs);
-     }(document, 'script', 'facebook-jssdk'));
-  $(window).ready(function(){
-      $('#loadingImg').show().delay(500).fadeOut(600); 
-  });
-
-
+    }(document, 'script', 'facebook-jssdk'));
+  	
 });
 
