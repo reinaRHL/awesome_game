03d80b6e89ed9a654ac782d98002bad1bdefebf3
James Araujo <jaraujo@sfu.ca>
2017-03-25 17:50:05 -0700

    signup post + login session working

--- web-app/app/src/app.js
+++ web-app/app/src/app.js
@@ -2,12 +2,20 @@ var bodyParser = require('body-parser');
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+var models = require('./models');
+var crypto = require('crypto');
+
+
+
 var app = express();
 var PORT = 3000;
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({   // to support URL-encoded bodies
   extended: true
 }));
+app.use(cookieParser());
 
 // Middleware
 app.use(morgan('dev'));
@@ -39,8 +47,79 @@ io.on('connection', function (socket) {
 });
 
 
-/// This is for chatting feature 
+/// This is for chatting feature
 /// there's separate chat.html but it can be incoporated into lobby later
+
+app.post('/signup', function(req,res){
+  var uname = req.body['username'];
+  var pass = req.body['password'];
+
+  //create new user under form credentials
+    models.User.create({
+      username: uname,
+      password: bcrypt.hashSync(pass, 10), //bcrypt hash password
+      gamesWon: 0,
+      gamesPlayed: 0,
+      lastLoggedIn: null,
+    }).then(function (new_user){
+      //created new user
+      res.status(200).end();
+    });
+  });
+
+
+
+
+
+app.post('/login', function(req, res){
+  var uname = req.body['username'];
+  var pass = req.body['password'];
+
+  models.User.count({
+    where:{
+      username: uname
+    }
+  }).then(function(found){
+    if(found == 1){
+      models.User.findOne({
+        where:{
+          username: uname
+        }
+      }).then(function(nextstep){
+
+
+      bcrypt.compare(pass, nextstep.password, function(err, res2){
+        if(res2){
+          // correct credentials
+          var generateKey = function() {
+          var sha = crypto.createHash('sha256');
+          sha.update(Math.random().toString());
+          return sha.digest('hex');
+          };
+
+          models.Session.create({
+            key: generateKey()
+          }).then(function (send_the_response){
+            res.cookie('key', send_the_response.key);
+            res.setHeader("Content-Type", "application/json; charset=UTF-8");
+            res.status(200);
+            res.send(JSON.stringify('/friends')); //redirect
+            console.log("That user is in DB");
+        });
+      }else{
+        res.status(401);
+        res.end();
+      }
+      }); // bcrypt
+      });
+    }
+    else{
+      res.status(401);
+      res.end();
+    }
+  });
+
+});
 
 app.get('/', function(req,res){
   res.status(301);
@@ -56,11 +135,12 @@ app.get('/login', function(req, res){
   res.sendFile(path.resolve('../static/login.html'));
 });
 
+
 app.get('/js/login.js', function(req, res){
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/javascript; charset=UTF-8');
   res.setHeader('Cache-Control', 'max-age=1800');
-    
+
   res.sendFile(path.resolve('../static/js/login.js'));
 });
 
@@ -68,7 +148,7 @@ app.get('/js/chat.js', function(req, res){
 	res.statusCode = 200;
   res.setHeader('Content-Type', 'application/javascript; charset=UTF-8');
   res.setHeader('Cache-Control', 'max-age=1800');
-    
+
 	res.sendFile(path.resolve('../static/js/chat.js'));
 });
 
@@ -76,7 +156,7 @@ app.get('/js/template.js', function(req, res){
 	res.statusCode = 200;
     res.setHeader('Content-Type', 'application/javascript; charset=UTF-8');
     res.setHeader('Cache-Control', 'max-age=1800');
-    
+
 	res.sendFile(path.resolve('../static/js/template.js'));
 });
 
@@ -100,7 +180,7 @@ app.get('/css/custom.css', function(req, res){
   res.setHeader('Cache-Control', 'max-age=1800');
   res.sendFile(path.resolve('../static/css/custom.css'));
 });
-   
+
 
 app.get('/img/avatar.png', function(req, res){
 	res.statusCode = 200;
@@ -108,6 +188,3 @@ app.get('/img/avatar.png', function(req, res){
     res.setHeader('Cache-Control', 'max-age=1800');
     res.sendFile(path.resolve('../static/img/avatar.png'));
 });
-
-
-
