d73fb8f5a745b8926ba4e88a94d9be4827d563bc
Abram Wiebe <awiebe@burningthumb.com>
2017-03-26 20:34:34 -0700

    Added node service placeholders

--- chef/cookbooks/baseconfig/recipes/default.rb
+++ chef/cookbooks/baseconfig/recipes/default.rb
@@ -35,3 +35,46 @@ mysql_service 'mysql' do
   initial_root_password 'hello'
   action [:create, :start,:restart]
 end
+
+
+###NodeJS
+# Add repository so apt-get can install latest Node from NodeSource
+execute "add_nodesource_repo" do
+  command "curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -"
+end
+ 
+# Install node.js
+package "nodejs"
+ 
+# Install sqlite
+package "sqlite"
+ 
+# Install package dependencies and run npm install
+execute "npm_install" do
+  cwd "/home/ubuntu/project/web-app"
+  command "sudo npm install -g node-pre-gyp"
+  command "npm install --no-bin-links"
+end
+###END NodeJS
+
+
+###Begin game app
+# Populate the DB
+execute "populate_db" do
+  cwd "/home/ubuntu/project/web-app"
+  command "node populateDb.js"
+end
+# Add a service file for running the music app on startup
+cookbook_file "musicapp.service" do
+    path "/etc/systemd/system/musicapp.service"
+end
+ 
+# Start the music app
+execute "start_musicapp" do
+    command "sudo systemctl start musicapp"
+end
+ 
+# Start music app on VM startup
+execute "startup_musicapp" do
+    command "sudo systemctl enable musicapp"
+end
