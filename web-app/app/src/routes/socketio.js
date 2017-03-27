ea0dbe81e0104fbfe7bc0d78b936d12b67552184
Jay Svoboda <jay@svoboda.co>
2017-03-27 13:11:08 -0700

    fix indentation in socketio.js

--- web-app/app/src/routes/socketio.js
+++ web-app/app/src/routes/socketio.js
@@ -1,86 +1,82 @@
 var models = require('../models');
 
 module.exports = function (server) {
-    var io = require('socket.io')(server);
-    var connections = [];
+	var io = require('socket.io')(server);
+	var connections = [];
 
-    //io user Authentication
-    function getCookie(cookie, cname) {
-        var name = cname + "=";
-        var ca = cookie.split(';');
-        for (var i = 0; i < ca.length; i++) {
-            var c = ca[i];
-            while (c.charAt(0) == ' ') {
-                c = c.substring(1);
-            }
-            if (c.indexOf(name) == 0) {
-                return c.substring(name.length, c.length);
-            }
-        }
-        return "";
-    }
+	//io user Authentication
+	function getCookie(cookie, cname) {
+		var name = cname + "=";
+		var ca = cookie.split(';');
+		for (var i = 0; i < ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0) == ' ') {
+				c = c.substring(1);
+			}
+			if (c.indexOf(name) == 0) {
+				return c.substring(name.length, c.length);
+			}
+		}
+		return "";
+	}
 
-    io.use(function(socket, next) {
-        
+	io.use(function(socket, next) {
+		var seshKey = getCookie(socket.request.headers.cookie, "key");
 
-        var seshKey = getCookie(socket.request.headers.cookie, "key");
+		models.Session.count({
+			where: {
+				key: seshKey
+			}
+		}).then(function(session) {
+			if (session === 1) {
+				models.Session.findOne({
+					where: {
+						key: seshKey
+					}
+				}).then(function(session) {
+					models.User.findOne({
+						where: {
+							id: session.user_id
+						}
+					}).then(function(user) {
+						io.sockets.emit('onlineUser', {name:user.username});
+					});
+				})
+				next();
+			} else {
+				// IF A USER ISN'T LOGGED IN, THEN REDIRECT THEM LOGIN
+				next(new Error('Authentication error'));
+			}
+		});
+	});
 
-        models.Session.count({
-                where: {
-                    key: seshKey
-                }
-            })
-            .then(function(session) {
-            if(session == 1){
-                models.Session.findOne({
-                    where: {
-                        key: seshKey
-                    }
-                }).then(function(session) {
-                    models.User.findOne({
-                    where: {
-                        id: session.user_id
-                    }
-                    }).then(function(user) {
-                    io.sockets.emit('onlineUser', {name:user.username});
-                    });
-                })
-                next();
-            }else{
-        // IF A USER ISN'T LOGGED IN, THEN REDIRECT THEM LOGIN
-                next(new Error('Authentication error'));      
-            }
-            });
-    });
+	io.on('connection', function (socket) {
+		connections.push(socket);
+		console.log("Connected: %s sockets connected", connections.length);
 
-    io.on('connection', function (socket) {
-        connections.push(socket);
-        console.log("Connected: %s sockets connected", connections.length);
+		socket.on('disconnect', function (data) {
+			connections.splice(connections.indexOf(socket),1);
+			console.log("Disconnected: %s sockets connected", connections.length);
+		});
 
-        socket.on('disconnect', function (data) {
-            connections.splice(connections.indexOf(socket),1);
-            console.log("Disconnected: %s sockets connected", connections.length);
-        });
-
-        // send message in Chatroom
-        // need to fix this to pass the real username
-        socket.on('sendMessage', function(data){
-        console.log(socket.request.headers.cookie+ "cookie is")
-        var seshKey = getCookie(socket.request.headers.cookie, "key");
-        models.Session.findOne({
-                where: {
-                    key: seshKey
-                }
-            }).then(function(session) {
-                models.User.findOne({
-                where: {
-                    id: session.user_id
-                }
-                }).then(function(user) {
-                io.sockets.emit('newMessage', {msg:data, name:user.username});
-                });
-            })
-            
-        })
-    });
+		// send message in Chatroom
+		// need to fix this to pass the real username
+		socket.on('sendMessage', function (data) {
+		console.log(socket.request.headers.cookie + "cookie is")
+		var seshKey = getCookie(socket.request.headers.cookie, "key");
+			models.Session.findOne({
+				where: {
+					key: seshKey
+				}
+			}).then(function (session) {
+				models.User.findOne({
+					where: {
+						id: session.user_id
+					}
+				}).then(function(user) {
+					io.sockets.emit('newMessage', {msg:data, name:user.username});
+				});
+			});
+		});
+	});
 };
