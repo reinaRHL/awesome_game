9f0e58d3b05e621ccc24ee1e3642aaf97ab9a6a5
reina <reinal@sfu.ca>
2017-03-31 13:10:02 -0700

    added api for getting friends list

--- web-app/app/src/routes/api.js
+++ web-app/app/src/routes/api.js
@@ -28,8 +28,60 @@ api.getUser = function (req, res) {
 
 }; //end getUser
 
+
+// Returns a list of friend objects(containing id & username) of the current user.
 api.getUserFriends = function (req, res) {
-	// TODO
+
+	models.Session.findOne({ // find current users session
+		where: {
+			key: req.cookies.key
+		}
+	}).then(function(session_instance){
+		models.User.findOne({
+			where: {
+				id: session_instance.user_id
+			}
+		}).then(function (current_user){
+			// Find all, where status is 1 and either (UserId or FriendId) is equal to the current user id.
+			models.UserFriend.findAll({where: {status: 1, $or: [{UserId: current_user.id}, {FriendId: current_user.id}]}})
+			.then(function(friends){
+
+				var friendIds = []
+				if (friends.length == 0){
+					res.send([]);
+				}
+				for (var i = 0; i< friends.length; i++) {
+					var friendId = [];
+					var friendsArray = [];
+					// Find out which one (FriendId or UserId) is friend id
+					if (friends[i].UserId === current_user.id){
+						friendId = friends[i].friendId;
+					} else{
+						friendId = friends[i].UserId;
+					}
+					friendIds.push(friendId);
+
+					if (friendIds.length === friends.length){
+						
+						for (var i = 0; i < friendIds.length; i++) {
+							models.User.findOne({where: {id: friendIds[i]}})
+							.then(function(user){
+								var z = {
+									id: user.id,
+									name: user.username
+								};
+								friendsArray.push(z);
+								if (friendsArray.length === friendIds.length){
+									res.send(friendsArray);
+								}
+							});
+						} 
+					}
+				}
+			});
+		});
+	});
+
 }; //end getUserFriends
 
 api.getAllGames = function (req, res) {
