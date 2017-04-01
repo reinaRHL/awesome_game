7d56d5fe694cab00207f0efdec7ced0a11a8b812
Abram Wiebe <awiebe@burningthumb.com>
2017-04-01 13:35:08 -0700

    Create class skeletons.

---
+++ web-app/app/src/class/GameSession.js
@@ -0,0 +1,58 @@
+"use strict";
+
+
+var GameSession = function( notifier )
+{
+
+}
+
+GameSession.prototype.participants=[];
+
+GameSession.prototype.getParticipants = function () {
+  return [{session:"XYZ",id:1,username:"Alice"},
+  {session:"XYZ",id:1,username:"Bob",channel:"aSocketInstnace"}
+  ]
+}
+
+GameSession.prototype.addParticipants = function (session,username,channel) 
+{
+ //TODO scan participants and return false if a session is duplicated
+ //
+}
+
+GameSession.prototype.broadcast =function(stringifyable)
+{
+	//for each channel send stringifiable
+}
+
+GameSession.prototype.unicastUsername(username,stringifyable)
+{
+	
+}
+
+GameSession.prototype.unicastId =function(userid)
+{
+	
+}
+
+GameSession.prototype.unicastSession =function(sessionkey)
+{
+	
+}
+
+GameSession.prototype.broadcastState =function()
+{
+	
+}
+
+GameSession.prototype.getState =function()
+{
+	
+}
+
+GameSession.prototype.advanceState=function()
+{
+	//go to the next game state
+}
+
+module.exports = GameSession;
---
+++ web-app/app/src/class/Notify.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var Notify = function() {
+}
+
+
+Notify.prototype.broadcast =function(stringifyable)
+{
+	//for each channel send stringifiable
+}
+
+Notify.prototype.unicastUsername(username,stringifyable)
+{
+	
+}
+
+Notify.prototype.unicastId =function(userid)
+{
+	
+}
+
+Notify.prototype.unicastSession =function(sessionkey)
+{
+	
+}
+
+
+module.exports = Notify;
