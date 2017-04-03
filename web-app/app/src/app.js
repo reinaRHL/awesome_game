25c6ba7cc3eb17a63db0717dfa58829444f80a12
Lin Cong <lcong@sfu.ca>
2017-04-02 22:53:34 -0700

    fix routing

--- web-app/app/src/app.js
+++ web-app/app/src/app.js
@@ -42,7 +42,7 @@ app.get('/profile', authentication.isAuthenticated, function (req, res) {
 app.get('/lobby', authentication.isAuthenticated, function (req, res) {
 	res.sendFile(path.resolve('../static/index.html'));
 });
-app.get('/game', authentication.isAuthenticated, function (req, res) {
+app.get('/games', authentication.isAuthenticated, function (req, res) {
 	res.sendFile(path.resolve('../static/game.html'));
 });
 app.get('/friends', authentication.isAuthenticated, function (req, res) {
